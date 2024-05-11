e10604206850deb290dbb3840cc2e328f684de88
Tais Taschetto <taistaschetto@hotmail.com>
2024-05-11 10:15:01 +0200

    fixed cursor, added links to other pages, fixed button

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const contactBtns = document.getElementsByClassName('contactButton');
     Array.from(contactBtns).forEach((btn) => {
         btn.addEventListener('click', function() {
-            window.location.href = 'contact.html'; // Adjust the URL as needed
+            window.open('contact.html', '_blank'); // Adjust the URL as needed
         });
     });
 });
